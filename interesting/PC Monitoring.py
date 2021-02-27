eb4f4228e0f485e4943fc960a43a0805f55ae244
LieWhy <47667489@qq.com>
2021-02-27 20:18:38 +0800

    Default Changelist

--- interesting/PC Monitoring.py
+++ interesting/PC Monitoring.py
@@ -2,19 +2,13 @@ import psutil
 import time
 from Function.boe_weixin_notice import boe_weixin_notice
 
-
-def mem_info():
-    mem = psutil.virtual_memory()
-    info = {'mem_total': mem[0], 'mem_free': mem[1], 'mem_percent': mem[2], 'mem_used': mem[3]}
-    return info
-
-
-res = mem_info()
-
-
-def main():
-    m_men = res
-    if m_men['men_percent'] > 50:
-        connect = ('内存使用超过%d' % m_men)
-        boe_weixin_notice.sendmessage("10286172", "Monitoring", connect)
-    time.sleep(60)
+try:
+    while True:
+        mail = boe_weixin_notice()
+        mem = psutil.virtual_memory()
+        if mem[2] > 50:
+            connect = mem[2]
+            mail.sendmessage("10286172", "Monitoring", "内存使用超过%d" % connect)
+        time.sleep(6)
+except ValueError:
+    print('获取值失败')
